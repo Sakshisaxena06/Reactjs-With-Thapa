6fecbb87369bb956336fa6ec802e835611814547
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-10-06 19:31:48 +0530

    counter

---
+++ 2_Counter/src/App.jsx
@@ -0,0 +1,7 @@
+import "./components/EV.css" ;
+import {State} from "./components/hooks/State";
+export const App =() =>{
+  return <section className="container">
+<State/>
+  </section>
+}
---
+++ 2_Counter/src/components/hooks/State.jsx
@@ -0,0 +1,14 @@
+export const State = () => {
+    let value =0;
+    const handleButtonClick =() =>{
+        value++;
+        console.log(value);
+        
+    }
+    return(
+        <>
+        <h1>{value}</h1>
+        <button onClick={handleButtonClick}>Increement</button>
+        </>
+    );
+};
