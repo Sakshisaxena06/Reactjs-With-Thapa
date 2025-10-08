8cc44f83484a19b980d10dcf76401c64a9d83765
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-10-08 18:24:03 +0530

    Counter

--- 2_Counter/src/components/hooks/State.jsx
+++ 2_Counter/src/components/hooks/State.jsx
@@ -1,14 +1,37 @@
+import React from "react";
+import { useState } from "react";
 export const State = () => {
-    let value =0;
-    const handleButtonClick =() =>{
-        value++;
-        console.log(value);
+//     let value =0;
+//     const handleButtonClick =() =>{
+//         value++;
+//         console.log(value);
         
-    }
+//     }
+const [count, setCount] = useState(5);
+console.log("Parent rendered");
+
+const handleButtonClick =()=>{
+    setCount(()=>count+1);
+    console.log(count);
+    
+}
+// console.log(count);
+
     return(
         <>
-        <h1>{value}</h1>
+      <div className="main-div">
+        <h1>{count}</h1>
         <button onClick={handleButtonClick}>Increement</button>
+      </div>
+      <ChildComponent/>
         </>
     );
-};
+}
+function ChildComponent() {
+console.log("Child Component rendered");
+return(<div className="main-div">
+ 
+<h2>Child Component</h2> 
+        </div>
+        ) 
+}
