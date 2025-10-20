89de15dff9ad6e9aab2600d6e2c5e0574f7fa2eb
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-10-20 10:36:42 +0530

    Hooks

--- 6_Hooks/src/components/useState/Challenge.jsx
+++ 6_Hooks/src/components/useState/Challenge.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
-
 import { useState } from "react";
-import "../../components/Hooks.css"
+import "../Hooks.css";
 
 const CounterChallenge = () => {
   const [count, setCount] = useState(0);
